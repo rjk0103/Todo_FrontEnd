14c2796e8d45a98dd7ade4aa68e4db198536923d
rjk0103 <jaekyoung.dev@gmail.com>
2024-11-01 19:04:33 +0900

    todo list front end 작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+import React, { useState, useEffect } from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from './AddTodo';
+import { call } from './service/ApiService';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    call("/todo", "GET", null)
+    .then((response) => setItems(response.data));
+  }, []);
+
+  const addItem = (item) => {
+    call("/todo", "POST", item)
+    .then((response) => setItems(response.data));
+  };
+
+  const deleteItem = (item) => {
+    call("/todo", "DELETE", item)
+    .then((response) => setItems(response.data));
+  };
+
+  const editItem = (item) => {
+    call("/todo", "PUT", item)
+    .then((response) => setItems(response.data));
+  };
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo 
+            item={item} 
+            key={item.id} 
+            editItem={editItem}
+            deleteItem={deleteItem}
+          />
+        ))}
+      </List>
+    </Paper>
+  );
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
     </div>
   );
 }
